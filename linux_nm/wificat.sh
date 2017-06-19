72c86cbaca7e29c5c95641f03f95b7e90d8e23f7
Bennet Becker <bennet@becker-dd.de>
2017-06-19 21:58:22 +0200

    adding connection via DBus

--- linux_nm/wificat.sh
+++ linux_nm/wificat.sh
@@ -1,8 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash
 
 SSID=sample-ssid
+
+SSID_TO_DELETE=sample-unsecure-ssid
+
+PAIRWISE=ccmp
+GROUP_CIPHER=ccmp
 ANON_ID=anonymous
-SUBJ_ALT_MATCH=
+SUBJ_ALT_MATCH=test.example.com
 CA_CERT="
 -----BEGIN CERTIFICATE-----
 MIIDnzCCAoegAwIBAgIBJjANBgkqhkiG9w0BAQUFADBxMQswCQYDVQQGEwJERTEc
@@ -29,14 +34,12 @@ Cm26OWMohpLzGITY+9HPBVZkVw==
 "
 DNS_SUFF_MATCH=sample-ssid.mpipks-dresden.mpg.de
 
-USE_CLIENT_CERT=1
+USE_CLIENT_CERT=0
 
 #Valid methods are: "leap", "md5", "tls", "peap", "ttls", "pwd", and "fast"
 EAP_METHOD=peap
 #NIY
 USE_EAP_PIN=0
-#"pap", "chap", "mschap", "mschapv2", "gtc", "otp", "md5", and "tls"
-PHASE2_METHOD_NONEAP=
 #"md5", "mschapv2", "otp", "gtc", and "tls"
 PHASE2_METHOD_EAP=mschapv2
 PHASE2_CA_CERT=
@@ -46,71 +49,122 @@ PHASE2_SUBJ_ALT_MATCH=
 PHASE2_USE_CLINET_CERT=0
 
 
+#Make sure we are running in bash
+if test -z "$BASH"
+then
+    bash $0
+    exit 0
+fi
+
 if test -n "$(which zenity)"
 then
-    LOGIN=$(zenity --title="$SSID Login" --forms --text="Your Wifi Login" --add-entry=Username --add-password=Password --separator=°)
+    LOGIN=$(zenity --title="$SSID Login" --forms --text="Your Wifi Login" --add-entry=Username \
+    --add-password=Password --separator=$'\n')
+    USERN=$(printf "$LOGIN" | head -n 1)
+    PASSWORD=$(printf "$LOGIN" | tail -n 1)
+
+    #echo $USERN $PASSWORD
     if test $USE_CLIENT_CERT -eq 1
     then
-        CLIENTCERT=$(zenity --title="Choose Client Certificate" --file-selection --file-filter="Certificate | *.p12 *.pem *.crt *.cert *.der" --file-filter="All Files | *.*")
+        CLIENTCERT=$(zenity --title="Choose Client Certificate" --file-selection \
+        --file-filter="Certificate | *.p12 *.pem *.crt *.cert *.der" --file-filter="All Files | *.*")
     fi
+elif test -n "$(which kdialog)"
+then
+    #TODO: Implement kdialog support
+    true
+elif test -n "$(which xdialog)"
+then
+    #TODO: Implement xdialog support
+    true
+elif test -n "$(which dialog)"
+then
+    #TODO: Implement dialog support
+    true
 else
     echo Falling back to readline
-    echo "Username: "
-    read -n USERN
-    echo "Password (not echoed): "
-    read -s PASSWORD
-    LOGIN=${USERN}°${PASSWORD}
+    read -p "Username: " USERN
+    read -p "Password (not echoed): " -s PASSWORD
 fi
 
-if test -n "$(which nmcli)"
+
+NM_VERSION=$(gdbus call --system --dest org.freedesktop.NetworkManager --object-path /org/freedesktop/NetworkManager \
+--method org.freedesktop.DBus.Properties.Get "org.freedesktop.NetworkManager" "Version" | grep -Po "(?<=<').*(?='>)")
+
+#TODO: jump to wpa_supplicant generation!
+test $(echo 0.8$'\n'$NM_VERSION | sort -Vr | tail -n 1) != 0.8
+
+if $? || ! pgrep -f NetworkManager
 then
-    nmcli con add type wifi con-name $SSID ifname wlp4s0 ssid $SSID
-    mkdir -p $HOME/.cat_installer
-    cat <<EOF > $HOME/.cat_installer/${SSID}.ca.pem
-$CA_CERT
-EOF
-    nmcli c modify $SSID 802-11-wireless-security.key-mgmt wpa-eap $(
-        echo 802-1x.identity $(echo $LOGIN | awk -F° '{ print $1}')
-        echo 802-1x.password $(echo $LOGIN | awk -F° '{ print $2}')
-        test -n "$ANON_ID"               && echo 802-1x.anonymous-identity $ANON_ID
-        test -n "$SUBJ_ALT_MATCH"        && echo 802-1x.altsubject-matches $SUBJ_ALT_MATCH
-        test -n "$CA_CERT"               && echo 802-1x.ca-cert $HOME/.cat_installer/${SSID}.ca.pem
-        test -n "$CLIENTCERT"            && echo 802-1x.client-cert $CLIENTCERT
-        test -n "$DNS_SUFF_MATCH"        && echo 802-1x.domain-suffix-match $DNS_SUFF_MATCH
-        test -n "$EAP_METHOD"            && echo 802-1x.eap $EAP_METHOD
-        test -n "$PHASE2_METHOD_NONEAP"  && echo 802-1x.phase2-auth $PHASE2_METHOD_NONEAP
-        test -n "$PHASE2_METHOD_EAP"     && echo 802-1x.phase2-autheap $PHASE2_METHOD_EAP
-        test -n "$PHASE2_CA_CERT"        && echo 802-1x.phase2-ca-cert $HOME/.cat_installer/${SSID}.ca2.pem
-        test -n "$PHASE2_DNS_SUFF_MATCH" && echo 802-1x.phase2-domain-suffix-match $PHASE2_DNS_SUFF_MATCH
-        test -n "$PHASE2_SUBJ_ALT_MATCH" && echo 802-1x.phase2-altsubject-matches $PHASE2_SUBJ_ALT_MATCH
-    )
-else
-    cat <<EOF >> $HOME/.wpa_supplicant.conf
-network={
-    ssid="$SSID"
-    scan_ssid=1
-    key_mgmt=WPA-EAP
-    pairwise=CCMP TKIP
-    group=CCMP TKIP
-    $( echo identity=\"$(echo $LOGIN | awk -F° '{ print $1}')\" )
-    $( echo password=\"$(echo $LOGIN | awk -F° '{ print $2}')\" )
-    $( test -n "$ANON_ID"               && echo anonymous_identity=\"$ANON_ID\" )
-    $( test -n "$SUBJ_ALT_MATCH"        && echo altsubject_matches=\"$SUBJ_ALT_MATCH\" )
-    $( test -n "$CA_CERT"               && echo ca_cert=\"$HOME/.cat_installer/${SSID}.ca.pem\" )
-    $( test -n "$CLIENTCERT"            && echo client_cert=\"$CLIENTCERT\" )
-    $( test -n "$DNS_SUFF_MATCH"        && echo domain_suffix_match=\"$DNS_SUFF_MATCH\" )
-    $( test -n "$EAP_METHOD"            && echo eap=$(echo $EAP_METHOD | tr '[a-z]' '[A-Z]') )
-    $( test -n "$PHASE2_METHOD_NONEAP"  && echo phase2=\"auth=$(echo $PHASE2_METHOD_NONEAP | tr '[a-z]' '[A-Z]')\" )
-    $( test -n "$PHASE2_METHOD_EAP"     && echo phase2=\"autheap=$(echo $PHASE2_METHOD_EAP | tr '[a-z]' '[A-Z]')\" )
-    $( test -n "$PHASE2_CA_CERT"        && echo ca_cert2=\"$HOME/.cat_installer/${SSID}.ca2.pem\" )
-    $( test -n "$PHASE2_DNS_SUFF_MATCH" && echo domain_suffix_match2=\"$PHASE2_DNS_SUFF_MATCH\" )
-    $( test -n "$PHASE2_SUBJ_ALT_MATCH" && echo altsubject_matches2=\"$PHASE2_SUBJ_ALT_MATCH\" )
-}
-EOF
-echo Config append to ~/.wpa_supplicant.conf
-echo Press any key to continue
-read
+    echo "Your NM version is not supported or no NM was found" 1>&2
+    read -p "Should we generate a wpa_supplicant? [Y/n] " GENWPASUPP
+
 fi
 
+# Get Connection Paths from DBus
+if test ${NM_VERSION} = 0.8
+then
+    NM_SETTINGS_OBJ=/org/freedesktop/NetworkManagerSettings
+    NM_SETTINGS_INTERFACE=org.freedesktop.NetworkManagerSettings
+else
+    NM_SETTINGS_OBJ=/org/freedesktop/NetworkManager/Settings
+    NM_SETTINGS_INTERFACE=org.freedesktop.NetworkManager.Settings
+fi
 
-exit 0
+NM_CONNECTIONS=$(gdbus call --system  --dest org.freedesktop.NetworkManager \
+    --object-path ${NM_SETTINGS_OBJ} --method ${NM_SETTINGS_INTERFACE}.ListConnections \
+    | grep -Po "(?<=')[^,].*?(?=')")
+
+# Delete existing connection
+for conn in $NM_CONNECTIONS
+do
+    conn_info=$(gdbus call --system  --dest org.freedesktop.NetworkManager --object-path ${conn}\
+     --method org.freedesktop.NetworkManager.Settings.Connection.GetSettings)
+    if test "$(echo ${conn_info} | grep -Po "(?<='type': \<').*?(?='\>)")" = "802-11-wireless"
+    then
+        conn_ssid=$(echo ${conn_info} | grep -Po "(?<='id': \<').*?(?='\>)")
+        if test ${conn_ssid} = ${SSID} || test ${conn_ssid} = ${SSID_TO_DELETE}
+        then
+            logger --id=$$ $(gdbus call --system  --dest org.freedesktop.NetworkManager --object-path ${conn}\
+                                --method org.freedesktop.NetworkManager.Settings.Connection.Delete || true) || true
+            logger --id=$$ "Connection ${conn} deleted!" || true
+        fi
+    fi
+done
+
+# Add new connection
+#
+uuid=$(cat /proc/sys/kernel/random/uuid)
+
+gdbus call --system --dest org.freedesktop.NetworkManager --object-path ${NM_SETTINGS_OBJ} \
+--method ${NM_SETTINGS_INTERFACE}.AddConnection \
+    '{
+        "connection":{
+            "type": <"802-11-wireless">,
+            "uuid": <"'$(cat /proc/sys/kernel/random/uuid)'">,
+            "permissions": <["user:'${USER}'"]>,
+            "id": <"'${SSID}'">
+        },
+        "802-11-wireless":{
+            "ssid": <b"test">,
+            "security": <"802-11-wireless-security">
+        },
+        "802-11-wireless-security":{
+            "key-mgmt": <"wpa-eap">,
+            "proto": <["rsn"]>,
+            "pairwise": <["'${PAIRWISE}'"]>,
+            "group": <["'${GROUP_CIPHER}'"]>
+        },
+        "802-1x": {
+            "eap": <["'${EAP_METHOD}'"]>,
+            "identity": <"'${USERN}'">,
+            "ca-cert": <[byte '$(openssl x509 -in <(echo "$CA_CERT") -inform pem -outform der | hexdump -ve '1/1 "0x%.2x,"'| head -c-1)']>,
+            '$(test ${NM_VERSION} = 0.8 && echo '"subject-match":<"'${SUBJ_ALT_MATCH}'">' || \
+                                         echo '"altsubject-matches":<["'${SUBJ_ALT_MATCH}'"]>')',
+            "password": <"'${PASSWORD}'">,
+            "phase2-auth": <"'${PHASE2_METHOD_EAP}'">,
+            "anonymous-identity": <"'${ANON_ID}'">
+        },
+        "ipv4": { "method": <"auto"> },
+        "ipv6": { "method": <"auto"> }
+    }'
